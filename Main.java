5fcf27f61d0e044cf14fe0ec10a3eca180829cf7
ElDestructor88 <evgenijlukancikov62@gmail.com>
2025-04-09 15:39:05 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static int calculate(String input) throws Exception {
         String[] parts = input.split(" ");
         if (parts.length != 3) {
-            throw new Exception("Invalid input format");
+            throw new Exception("Ошибка: неверный формат ввода");
         }
 
         int a;
@@ -25,11 +25,11 @@ public class Main {
             a = Integer.parseInt(parts[0]);
             b = Integer.parseInt(parts[2]);
         } catch (NumberFormatException e) {
-            throw new Exception("Invalid number format");
+            throw new Exception("Ошибка: неверный формат ввода чисел");
         }
 
         if (a < 1 || a > 10 || b < 1 || b > 10) {
-            throw new Exception("Numbers must be between 1 and 10");
+            throw new Exception("Ошибка: превышен числовой диапазон");
         }
 
         String operator = parts[1];
@@ -43,11 +43,11 @@ public class Main {
                 return a * b;
             case "/":
                 if (b == 0) {
-                    throw new Exception("Division by zero");
+                    throw new Exception("Ошибка: ноль в качестве делителе");
                 }
                 return a / b;
             default:
-                throw new Exception("Invalid operator");
+                throw new Exception("Ошибка: неизвестная операция");
         }
     }
 }
